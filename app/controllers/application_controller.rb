14cda54efa93a8dda7a157f2d02aa6f7cbe54799
Lucille <lucille.lacroix74520@gmail.com>
2025-09-02 17:33:10 +0200

    add username/ missing link and front end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-end
+  before_action :configure_permitted_parameters, if: :devise_controller?#step one to change automatique form of registration (+ username)
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end # ajoute username to the formulaire 
+  end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-
   def home
+    if user_signed_in?
+      @user = current_user
+    end
   end
 end
