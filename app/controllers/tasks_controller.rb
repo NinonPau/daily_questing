56ee293deb782da49a93476737ddb34e658ad04d
Ninon Pau <ninon.pau@protonmail.com>
2025-09-02 15:11:28 +0200

    Add the create part of #random

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,6 +19,13 @@ class TasksController < ApplicationController
     url = "https://bored.api.lewagon.com/api/activity"
     activity_serialized = URI.parse(url).read
     activity = JSON.parse(activity_serialized)
+    @task = Task.new(name: activity["activity"], description: "Type of quest: #{activity["type"]} - Number of participants recommended: #{activity["participants"]}")
+    raise
+    if @task.save
+      redirect_to tasks_path, notice: "Quest successfully created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
