432bd8a209ab479e0df72c190e6dc4124d257d1c
Lucille <lucille.lacroix74520@gmail.com>
2025-09-02 12:42:20 +0200

    add view and controllers for ADD S.Quest

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,21 @@
 class TasksController < ApplicationController
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to new_task_path,  notice: "Quest successfully created!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :daily, :xp)
+  end
 end
