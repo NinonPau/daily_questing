3714c5883c17ea0106155053252364909423e53b
Ninon Pau <ninon.pau@protonmail.com>
2025-09-02 12:35:11 +0200

    Add #random: route, action (in tasks controller)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,10 @@
+require "json"
+require "open-uri"
+
 class TasksController < ApplicationController
+  def random
+    url = "https://bored.api.lewagon.com/api/activity"
+    activity_serialized = URI.parse(url).read
+    activity = JSON.parse(activity_serialized)
+  end
 end
