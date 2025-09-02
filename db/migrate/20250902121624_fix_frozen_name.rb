052f25520fab93ce0c3cf3fe3a5053edc01532e3
Ninon Pau <ninon.pau@protonmail.com>
2025-09-02 16:29:17 +0200

    Add new models with migration files

--- db/migrate/20250902121624_fix_frozen_name.rb
+++
@@ -1,5 +0,0 @@
-class FixFrozenName < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :user_tasks, :frozen, :ignored
-  end
-end
--- db/migrate/20250902141226_create_tasks.rb
+++ db/migrate/20250902141226_create_tasks.rb
@@ -1,11 +1,13 @@
 class CreateTasks < ActiveRecord::Migration[7.1]
   def change
     create_table :tasks do |t|
+      t.references :user, null: false, foreign_key: true
       t.string :name
       t.text :description
       t.boolean :daily
-      t.boolean :duo
+      t.boolean :completed
       t.float :xp
+      t.boolean :duo
 
       t.timestamps
     end
