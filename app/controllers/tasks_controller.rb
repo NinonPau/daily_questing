c13f773f5d4f6165caae002a18e6039358941e84
Ninon Pau <ninon.pau@protonmail.com>
2025-09-03 12:06:36 +0200

    Add link to the description when creating a random task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,11 +25,17 @@ class TasksController < ApplicationController
     url = "https://bored.api.lewagon.com/api/activity"
     activity_serialized = URI.parse(url).read
     activity = JSON.parse(activity_serialized)
-    @task = current_user.tasks.new(id: current_user.id, name: activity["activity"], description: "Type of quest: #{activity["type"]} - Number of participants recommended: #{activity["participants"]}", xp: 20)
+    @task = current_user.tasks.new(
+      user_id: current_user.id,
+      name: activity["activity"],
+      description: "Type of quest: #{activity["type"]} -
+                    Number of participants recommended: #{activity["participants"]}
+                    #{activity["link"] == "" ? "" : " - Link: #{activity["link"]}"}",
+      xp: 20)
     if @task.save
       redirect_to tasks_path, notice: "Quest successfully created!"
     else
-      render :new, status: :unprocessable_entity
+      render :home, status: :unprocessable_entity
     end
   end
 
