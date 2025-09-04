15ad3522974552b3adfcda9cc4a356a171a8c05a
Ninon Pau <ninon.pau@protonmail.com>
2025-09-04 11:34:21 +0200

    Update user_moods_controller.rb

--- app/controllers/user_moods_controller.rb
+++ app/controllers/user_moods_controller.rb
@@ -1,21 +1,9 @@
 class UserMoodsController < ApplicationController
   before_action :authenticate_user!
+  
   def index
-    # @moods = UserMood.all
-    @user_mood = current_user.user_mood || UserMood.new(user: current_user)
+    @user_mood = current_user.user_mood
   end
-
-
-  def create
-    @user_mood = UserMood.new(user_mood_params)
-    @user_mood.user = current_user
-      if @user_mood.save
-        redirect_to user_moods_path, notice: "Mood selected successfully!"
-      else
-        render :index, status: :unprocessable_entity
-      end
-    end
-  #patch method to update the user_mood based on the mood_type
 
   def update
     @user_mood = UserMood.find(params[:id])
@@ -23,7 +11,6 @@ class UserMoodsController < ApplicationController
     @user_mood.save
     redirect_to root_path
   end
-
 
   private
 
