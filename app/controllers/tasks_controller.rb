f15eef6c9fad30d427985fd2881bb7f1ed025ba8
Lucille <lucille.lacroix74520@gmail.com>
2025-09-09 02:28:39 +0200

    final debugging

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -65,8 +65,15 @@ class TasksController < ApplicationController
 
   def complete
     if @task.update(completed: true)
+      # Give XP to the user completing the task
       current_user.add_xp(@task.xp || 0)
-      redirect_to tasks_path, notice: "Congratulations, You completed the quest '#{@task.name}'!"
+
+      # Give XP to all accepted participants
+      @task.task_participants.where(status: "accepted").each do |tp|
+        tp.user.add_xp(@task.xp || 0)
+      end
+
+      redirect_to tasks_path, notice: "Congratulations, Quest '#{@task.name}' completed!"
     else
       redirect_to tasks_path, alert: "Could not complete the quest."
     end
