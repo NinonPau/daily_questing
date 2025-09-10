b8a70df9338f1aa28f7a8147720fd71b07382b56
Renata Faltova <renata.97@seznam.cz>
2025-09-10 08:32:19 +0100

    fix the bug

--- app/models/task.rb
+++ app/models/task.rb
@@ -10,7 +10,7 @@ class Task < ApplicationRecord
   end
 
   def self.reset_for_today # task to reset the ToDo list
-    Task.where.not(date: Date.today).delete_all # delette the task of the date
+    Task.where.not(date: Date.today).destroy_all # delette the task of the date
     #may want to make if statement if we want to keep random task
     User.find_each do |user|
       user.tasks.where(daily: true).each do |daily_task|
